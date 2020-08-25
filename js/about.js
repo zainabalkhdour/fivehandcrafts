a37205326d51c38586db4b7dff19a8aa18e4e8e5
zainabalkhdour <zainab.alkhdour@gmail.com>
2020-08-26 01:30:11 +0300

    js

--- js/about.js
+++ js/about.js
@@ -6,12 +6,12 @@ console.log('what is your name?',name);
 console.log('how old are you?',age);
 console.log('this is the date today',date);
 
-alert = date
+
 var name = prompt('what is your name?: ');
 var age = prompt('how old are you?: ');
 console.log ('name\  's what is your name?;name);
 console.log ('age\  's how old are you?;age);
-var date = prompt('this is the date today');
+var date = confirm('this is the date today');
 console.log ('this is the date today',date);
 
 
