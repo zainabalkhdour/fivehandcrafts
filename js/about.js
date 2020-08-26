0cd38ed5916f3dc7673b22ac34c778279ac0378b
zainabalkhdour <zainab.alkhdour@gmail.com>
2020-08-26 11:24:02 +0300

    js

--- js/about.js
+++ js/about.js
@@ -7,12 +7,14 @@ console.log('how old are you?',age);
 console.log('this is the date today',date);
 
 
-var name = prompt('what is your name?: ');
-var age = prompt('how old are you?: ');
+var name1 = prompt('what is your name?: ');
+var age1 = prompt('how old are you?: ');
 
 
-var date = confirm('this is the date today');
+var date1 = confirm('this is the date today',date);
 console.log ('this is the date today',date);
+
+alert ("are you zainab?");
 
 
 
